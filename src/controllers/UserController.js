9ecdc222bda8109a0612a883542d5d11a23043be
Romilo Dev <amiguinhos.rodrigo@gmail.com>
2020-05-09 00:17:52 +0100

    Bug Fix

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -9,27 +9,39 @@ const Bot = require('../models/Bot')
 module.exports = {
   async show(req, res) {
     const { user_id: user_token } = req.headers
-    const { user: user_id } = req.query
-
-    let tokens
+    let { user: user_id } = req.query
 
     if (user_token) {
-      tokens = await User.findById(user_token)
+      const tokens = await User.findById(user_token)
     
       if (!tokens) return res.status(401).json({ error: 401, message: "Unauthorized" })
-    } else {
-      tokens = await User.findOne({ id: user_id })
 
-      if (!tokens) return res.status(401).json({ error: 401, message: "invalid-id" })
+      user_id = tokens.id
     }
 
-    const user = await oauth.getUser(tokens.access_token)
+    const userinfo = await client.users.fetch(user_id)
 
-    user.avatar = user.avatar ?
-    `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png` :
-    `https://cdn.discordapp.com/embed/avatars/${user.discriminator % 5}.png`
+    const user = {
+      id: userinfo.id,
+      username: userinfo.username,
+      discriminator: userinfo.discriminator,
+      avatar: userinfo.displayAvatarURL(),
+      status: userinfo.presence.status,
+      streaming: userinfo.presence.activities.length > 0 && userinfo.presence.activities.filter(p => p.type === "STREAMING").length > 0
+    }
 
-    const bots = await Bot.find({ owner: user.id })
+    const customstatus = userinfo.presence.activities.filter(a => a.type === "CUSTOM_STATUS")[0]
+
+    user.customstatus = customstatus ? {
+      emoji: customstatus.emoji ? {
+        name: customstatus.emoji.name,
+        id: customstatus.emoji.id,
+        url: customstatus.emoji.url,
+      } : null,
+	  text: customstatus.state
+    } : null
+
+    const bots = await Bot.find({ owner: user_id })
 
     user.bots = []
 
@@ -49,27 +61,6 @@ module.exports = {
 
       user.bots.push(bot)
     }
-
-    const userinfo = await client.users.fetch(user.id)
-
-    user.status = userinfo.presence.status
-    user.streaming = userinfo.presence.activities.length > 0 && userinfo.presence.activities.filter(p => p.type === "STREAMING").length > 0
-
-    const customstatus = userinfo.presence.activities.filter(a => a.type === "CUSTOM_STATUS")[0]
-
-    user.customstatus = customstatus ? {
-      emoji: customstatus.emoji ? {
-        name: customstatus.emoji.name,
-        id: customstatus.emoji.id,
-        url: customstatus.emoji.url,
-      } : null,
-	  text: customstatus.state
-    } : null
-	
-	user.flags = undefined
-	user.public_flags = undefined
-	user.locale = undefined
-	user.mfa_enabled = undefined
 
     const member = client.guilds.cache.get('681445540005019688').members.cache.get(user.id)
     
